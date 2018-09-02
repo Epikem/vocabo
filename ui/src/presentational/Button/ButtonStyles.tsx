fcd80f6125d457b212392c64a941e9e8980558dd
Epikem <dltldls95@naver.com>
2018-09-02 13:56:29 +0900

    add button style

---
+++ ui/src/presentational/Button/ButtonStyles.tsx
@@ -0,0 +1,24 @@
+import { darken, lighten } from 'polished';
+import styled from "styled-components";
+
+const ButtonBox = styled.button`
+  background-color: ${props => props.theme.primaryColor};
+  border: 1px solid ${props => darken(0.1, props.theme.primaryColor)};
+  padding: .5rem;
+  color: ${props => props.theme.fontColor};
+  font-size: ${props => props.theme.fontSize};
+  transition: all .15s linear;
+  transition-property: box-shadow, height, width, background-color, font-size, color;
+
+  :hover{
+    transition: background-color .15s linear;
+    background-color: ${props => lighten(0.1, props.theme.primaryColor)};
+  }
+
+  :active{
+    transition: background-color .01s linear;
+    background-color: ${props => darken(0.1, props.theme.primaryColor)};
+  }
+`
+
+export { ButtonBox };
