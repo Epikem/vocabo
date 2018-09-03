252c5546ff387941b34a40cc9fad6a3bbd781b92
Epikem <dltldls95@naver.com>
2018-09-03 20:10:36 +0900

    add Counter container

---
+++ ui/src/containers/Counter.ts
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import { bindActionCreators, Dispatch } from "redux";
+import { Counter } from "../presentational";
+import { increment } from './../store/counters';
+
+const mapStateToProps = (state: any) => ({
+  count: state.counter.reduxCounter,
+})
+
+const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
+  onIncrement: increment,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
