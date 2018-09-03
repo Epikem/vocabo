57a8480f66317d34d36241bf3a0d2cdf125d5387
Epikem <dltldls95@naver.com>
2018-09-03 16:16:30 +0900

    add redux store

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -1,4 +1,33 @@
-import * as redux from 'redux';
-import * as reactredux from 'react-redux';
-import * as r from 'ramda';
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { CountersAction, CountersReducer } from "./counters";
 
+import { StateType } from "typesafe-actions";
+// import { RouterAction, LocationChangeAction } from 'react-router-redux';
+// type ReactRouterAction = RouterAction | LocationChangeAction;
+
+export type RootState = StateType<typeof rootReducer>;
+// export type RootAction = ReactRouterAction | CountersAction;
+export type RootAction = CountersAction;
+
+// import { composeEnhancers } from './utils';
+// import { routerReducer } from 'react-router-redux';
+
+const rootReducer = combineReducers({
+  // router: routerReducer,
+  counters: CountersReducer
+});
+
+function configureStore(initialState?: object) {
+  // compose enhancers
+  // const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+  // create store
+  // return createStore(rootReducer, initialState!, enhancer);
+  return createStore(rootReducer, initialState!);
+}
+
+// pass an optional param to rehydrate state on app start
+const store = configureStore();
+
+// export store singleton instance
+export default store;
