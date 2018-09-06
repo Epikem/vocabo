87a278f26ebc0fdfa833c1c95d6b8ab947601efe
Epikem <dltldls95@naver.com>
2018-09-06 18:31:08 +0900

    add name property to theme object

--- ui/src/theme/index.ts
+++ ui/src/theme/index.ts
@@ -8,20 +8,23 @@ const {
 } = styledComponents as styledComponents.ThemedStyledComponentsModule<IThemeInterface>;
 
 export interface IThemeInterface {
-  primaryColor: string;
   fontColor: string,
   fontSize: string;
+  name: string;
+  primaryColor: string;
 }
 
 export const lightTheme = {
   fontColor: 'black',
   fontSize: '15px',
+  name: 'light',
   primaryColor: "#e9e9eb",
 };
 
 export const darkTheme = {
   fontColor: 'white',
   fontSize: '15px',
+  name: 'dark',
   primaryColor: "gray",
 };
 
