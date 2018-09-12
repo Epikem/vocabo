8e9c94a879aa60206449676d899fc13b2aa8cf19
Epikem <dltldls95@naver.com>
2018-09-12 20:37:37 +0900

    add NavItemStyles

---
+++ client/src/presentational/NavItem/NavItemStyles.tsx
@@ -0,0 +1,8 @@
+import { NavLink } from 'react-router-dom';
+import styled from "styled-components";
+
+export const NavItemBox = styled(NavLink)`
+  padding-left: 15px;
+  user-select: none;
+  cursor: pointer;
+`;
