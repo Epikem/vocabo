eba9b85039acca7cda97906e2de71d11e630076c
Epikem <dltldls95@naver.com>
2018-09-13 21:40:29 +0900

    add styled to Header

--- client/src/presentational/Header/HeaderStyles.tsx
+++ client/src/presentational/Header/HeaderStyles.tsx
@@ -4,16 +4,22 @@ import styled from "styled-components";
 const HeaderBox = styled.div`
   background-color: ${props => lighten(0.1,props.theme.primaryColor)};
   border: 1px solid ${props => darken(0.1, props.theme.primaryColor)};
-  padding: 1rem;
-  color: ${props => props.theme.fontColor};
-  font-size: ${props => props.theme.titleSize};
 `
 
 const LogoBox = styled.div`
-  font-size: ${props => props.theme.titleSize};
-  color: ${props => props.theme.fontColor};
   transition: all .15s linear;
   transition-property: box-shadow, height, width, background-color, font-size, color;
 `
 
-export { HeaderBox, LogoBox };
+const HeaderItem = styled.div`
+  color: ${props => props.theme.fontColor};
+  font-size: ${props => props.theme.titleSize};
+  padding: .5rem;
+  border-bottom: 1px solid transparent;
+
+  :hover{
+    border-bottom: 1px solid red;
+  }
+`
+
+export { HeaderBox, LogoBox, HeaderItem };
--- client/src/presentational/Header/index.tsx
+++ client/src/presentational/Header/index.tsx
@@ -1,15 +1,21 @@
-import * as React from 'react';
-import { HeaderBox, LogoBox } from './HeaderStyles';
+import * as React from "react";
+import { HeaderBox, LogoBox, HeaderItem } from "./HeaderStyles";
+import { FlexBox } from "../FlexBox";
+import { NavItem } from "../NavItem";
 
-interface IHeaderProps {
+interface IHeaderProps {}
 
-}
-
-export const Header: React.SFC<IHeaderProps> = (props) => {
+export const Header: React.SFC<IHeaderProps> = props => {
   return (
     <HeaderBox>
-      <LogoBox>Memorybook</LogoBox>
-      
+      <FlexBox direction="row">
+        <HeaderItem>
+          <LogoBox><NavItem to="/">Memorybook</NavItem></LogoBox>
+        </HeaderItem>
+        <HeaderItem>
+          <NavItem to="theme">theme</NavItem>
+        </HeaderItem>
+      </FlexBox>
     </HeaderBox>
   );
 };
