f172c0c98063d6dcdc2671fd145f67dad4253b5e
Epikem <dltldls95@naver.com>
2018-09-15 23:07:38 +0900

    add effects to NavItem style

--- client/src/presentational/NavItem/NavItemStyles.tsx
+++ client/src/presentational/NavItem/NavItemStyles.tsx
@@ -1,8 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import styled from "styled-components";
+import { theme } from '../../theme';
 
 export const NavItemBox = styled(NavLink)`
   padding-left: 15px;
   user-select: none;
   cursor: pointer;
+  font-size: inherit;
+
+  :hover{
+    color: ${theme('fontColor', 'hoverEffect')};
+  }
+
+  :active{
+    color:${theme('fontColor', 'activeEffect')}
+  }
 `;
