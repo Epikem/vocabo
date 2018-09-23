41f18bc92a5dc30a20b1a618b1d481eab17ef04f
Epikem <dltldls95@naver.com>
2018-09-23 19:52:31 +0900

    merge theme effects and attribute types into IStaticTheme

--- client/src/theme/index.ts
+++ client/src/theme/index.ts
@@ -14,36 +14,36 @@ ITheme
 
 type Effect = (color: string) => string;
 
-type themeEffects = Record<themeEffectKey, Effect>;
+type themeName = 
+  | "light"
+  | "dark";
 
-type themeAttributes = Record<themeAttributeKey, string>;
-
-interface IThemeEffects {
-  sharedEffect1: Effect;
-  activeEffect: Effect;
-  hoverEffect: Effect;
-  invertEffect: Effect;
-}
-
-interface IThemeAttributes {
+interface IStaticTheme {
   headerHeight: string;
   titleSize: string;
   boxShadow: string;
   navFontColor: string;
   fontSize: string;
   themeName: themeName;
-  primaryColor: string;
-  fontColor: string;
+
+  sharedEffect1: Effect;
+  activeEffect: Effect;
+  hoverEffect: Effect;
+  invertEffect: Effect;
+
+  primaryColor?: string;
+  fontColor?: string;
 }
 
-export interface ITheme extends IThemeEffects, IThemeAttributes {}
+export interface ITheme extends Required<IStaticTheme> {}
 
 // color scheme : https://coolors.co/ffc093-ede580-a4af69-a5d37a-8aa399
 
 const sharedTheme = {
   headerHeight: "100%",
   titleSize: "30px",
-  boxShadow: `0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)}`
+  boxShadow: `0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)}`,
+  sharedEffect1:(str:string)=>'test'
 };
 
 const colors = {
@@ -53,26 +53,6 @@ const colors = {
   tertiary: "#8aa399",
   success: "#a5d37a"
 };
-
-const lightEffects: themeEffects = {
-  activeEffect: (color: string) => lighten(0.2, color),
-  hoverEffect: (color: string) => {
-    return darken(0.2, color);
-  },
-  invertEffect: (color: string) => {
-    return invert(color);
-  }
-};
-
-const darkEffects: themeEffects = {
-  activeEffect: (color: string) => {
-    return darken(0.3, color);
-  },
-  hoverEffect: (color: string) => {
-    return lighten(0.3, color);
-  },
-  invertEffect: (color: string) => {
-    return invert(color);
 
 const getTheme = _.memoize(calculateTheme);
 
@@ -105,23 +85,33 @@ function calculateTheme(name: themeName) : ITheme{
   };
 }
 
-export const lightTheme: ITheme = {
+export const lightTheme: IStaticTheme = {
   navFontColor: transparentize(0.2, darken("0.2", colors.secondary)),
-  fontColor: 'black',
   fontSize: "15px",
   themeName: "light",
-  primaryColor: 'colors.warning',
-  ...lightThemeEffects,
+  activeEffect: (color: string) => lighten(0.2, color),
+  hoverEffect: (color: string) => {
+    return darken(0.2, color);
+  },
+  invertEffect: (color: string) => {
+    return invert(color);
+  },
   ...sharedTheme,
 };
 
-export const darkTheme: ITheme = {
+export const darkTheme: IStaticTheme = {
   navFontColor: transparentize(0.2, lighten("0.2", invert(colors.secondary))),
-  fontColor: 'white',
   fontSize: "15px",
   themeName: "dark",
-  primaryColor: 'darken("0.1", invert(colors.primary))',
-  ...darkThemeEffects,
+  activeEffect: (color: string) => {
+    return darken(0.3, color);
+  },
+  hoverEffect: (color: string) => {
+    return lighten(0.3, color);
+  },
+  invertEffect: (color: string) => {
+    return invert(color);
+  },
   ...sharedTheme,
 };
 
