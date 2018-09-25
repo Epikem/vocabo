5ae70d03df4c23dff66d00465a4d6e60f886a5ad
Epikem <dltldls95@naver.com>
2018-09-25 20:44:54 +0900

    add luminance display to theme select page

--- client/src/presentational/ThemeSelect/index.tsx
+++ client/src/presentational/ThemeSelect/index.tsx
@@ -1,5 +1,6 @@
+import { getLuminance } from "polished";
 import * as React from "react";
-import { darkTheme, lightTheme, ThemeProvider, getTheme, } from "../../theme";
+import { darkTheme, getTheme, lightTheme, ThemeProvider, } from "../../theme";
 import { Button } from "../Button";
 
 const ThemeSelect = (props: any) => {
@@ -19,6 +20,9 @@ const ThemeSelect = (props: any) => {
       <ThemeProvider theme={getTheme('light')}>
         <Button onClick={changeThemeLight}>light theme</Button>
       </ThemeProvider>
+      <div>
+        luminance: {getLuminance(currentTheme.primaryColor)}
+      </div>
     </div>
   )
 };
