d9c8f34fb3b68b4c4c6f82c10208c562aac21f82
Epikem <dltldls95@naver.com>
2018-09-26 18:37:24 +0900

    use array type on button props

--- client/src/presentational/Button/Button.tsx
+++ client/src/presentational/Button/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ButtonBox } from './ButtonStyles';
 
-const Button = ({children, onClick}: {children: React.ReactNode, onClick?: React.MouseEventHandler}, props:React.ButtonHTMLAttributes<HTMLButtonElement>[]) => {
+const Button = ({children, onClick}: {children: React.ReactNode, onClick?: React.MouseEventHandler}, props:Array<React.ButtonHTMLAttributes<HTMLButtonElement>>) => {
 
   return (
     <ButtonBox onClick={onClick} {...props}>{children}</ButtonBox>
