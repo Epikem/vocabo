78db43471b9e77403e7f911482bed14fca7bc644
Epikem <dltldls95@naver.com>
2018-10-12 19:01:10 +0900

    remove redundant transition style rules from ButtonBox - already on global styles

--- client/src/presentational/Button/ButtonStyles.tsx
+++ client/src/presentational/Button/ButtonStyles.tsx
@@ -2,13 +2,11 @@ import { darken, invert, lighten, transparentize } from 'polished';
 import styled, { css } from '../../theme';
 
 const ButtonBox = styled.button`${({ theme }) => css`
-  background-color: ${props => props.theme.primaryColor};
+  background-color: ${theme.primaryColor};
   border: 1px solid ${theme.borderColor};
   padding: .5rem;
-  color: ${props => props.theme.fontColor};
-  font-size: ${props => props.theme.fontSize};
-  transition: all .15s linear;
-  transition-property: box-shadow, height, width, background-color, font-size, color;
+  color: ${theme.fontColor};
+  font-size: ${theme.fontSize};
 
   :hover{
     transition: background-color .15s linear;
