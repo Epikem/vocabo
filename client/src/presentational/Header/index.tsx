f3b10b098263d9c9fabfe04678b410b84041fa1e
Epikem <dltldls95@naver.com>
2018-10-15 21:56:20 +0900

    use HeaderSearch on Header

--- client/src/presentational/Header/index.tsx
+++ client/src/presentational/Header/index.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { FlexBox } from "../FlexBox";
+import { HeaderSearch } from "../HeaderSearch";
 import { NavItem } from "../NavItem";
 import { HeaderBox, LogoBox } from "./HeaderStyles";
 
-interface IHeaderProps {}
+export interface Props {
 
-export const Header: React.SFC<IHeaderProps> = props => {
-  return (
-    <HeaderBox>
-      <FlexBox direction="row">
-        <LogoBox><NavItem to="/">Memorybook</NavItem></LogoBox>
-        <NavItem to="theme">theme</NavItem>
-      </FlexBox>
-    </HeaderBox>
-  );
-};
+}
+
+export const Header : React.SFC<Props> = (props) => (
+  <HeaderBox>
+    <FlexBox direction="row">
+      <LogoBox><NavItem to="/">Memorybook</NavItem></LogoBox>
+      <NavItem to="theme">theme</NavItem>
+      <div style={{width: '20%'}}></div>
+      <HeaderSearch></HeaderSearch>
+      <div style={{width: '40%', marginLeft:'auto'}}></div>
+    </FlexBox>
+  </HeaderBox>
+)
