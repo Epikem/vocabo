b907935f383ed1e71220db783c2d93e3fe283a30
Epikem <dltldls95@naver.com>
2018-10-15 23:19:53 +0900

    change NavItem prop types - make NavItemProps extend react-router's LinkProps - add right option to make header item floatable to right side

--- client/src/presentational/NavItem/index.tsx
+++ client/src/presentational/NavItem/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
+import { LinkProps } from 'react-router-dom';
 import { NavItemBox } from './NavItemStyles';
 
-interface INavItemProps {
+interface NavItemProps extends LinkProps {
   to: string;
+  right?: boolean;
 }
 
-export const NavItem : React.SFC<INavItemProps> = (props) => {
+export const NavItem : React.SFC<NavItemProps> = ({to, children, right=false, ...rest}: NavItemProps) => {
   return (
-    <NavItemBox {...props}>
-      {props.children}
+    <NavItemBox to={to} right={right} {...rest}>
+      {children}
     </NavItemBox>
   );
 };
