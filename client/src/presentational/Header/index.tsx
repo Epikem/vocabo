a347c20389edd2503c08bafec170bd127233e668
Epikem <dltldls95@naver.com>
2018-10-18 22:57:55 +0900

    add search props to header components - Header and HeaderSearch

--- client/src/presentational/Header/index.tsx
+++ client/src/presentational/Header/index.tsx
@@ -5,17 +5,22 @@ import { NavItem } from "../NavItem";
 import { HeaderBox, LogoBox } from "./HeaderStyles";
 
 export interface Props {
-
+  searchText?: string;
+  onSearchTextChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-export const Header : React.SFC<Props> = (props) => (
-  <HeaderBox>
-    <FlexBox direction="row">
-      <LogoBox><NavItem to="/">Memorybook</NavItem></LogoBox>
-      <NavItem to="theme">theme</NavItem>
-      <div style={{width: '20%'}}></div>
-      <HeaderSearch></HeaderSearch>
-      <div style={{width: '40%', marginLeft:'auto'}}></div>
-    </FlexBox>
-  </HeaderBox>
-)
+export const Header : React.SFC<Props> = (props) =>{ 
+  const { onSearchTextChange } = props;
+  return (
+    <HeaderBox>
+      <FlexBox direction="row">
+        <LogoBox><NavItem to="/">Memorybook</NavItem></LogoBox>
+        <NavItem to="theme">theme</NavItem>
+        <div style={{width: '20%'}}></div>
+        <HeaderSearch onChange={onSearchTextChange} text={props.searchText}></HeaderSearch>
+        <div style={{width: '40%', marginLeft:'auto'}}></div>
+      </FlexBox>
+    </HeaderBox>
+  )
+
+}
--- client/src/presentational/HeaderSearch/index.tsx
+++ client/src/presentational/HeaderSearch/index.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
+import { SearchActions } from "../../store/search";
 import { HeaderSearchInput } from "./HeaderSearchStyles";
 
-interface IHeaderSearchProps { }
+interface IHeaderSearchProps {
+  text?: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
 
 export const HeaderSearch: React.SFC<IHeaderSearchProps> = props => {
   return (
-    <HeaderSearchInput placeholder={'search words...'} />
+    <HeaderSearchInput
+      onChange={props.onChange}
+      value={props.text}
+      placeholder={"search words..."}
+    />
   );
 };
