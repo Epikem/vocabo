d085fd86a2e64e2c930c82a6b43440a8f96f5a6b
Epikem <dltldls95@naver.com>
2018-10-21 20:31:41 +0900

    simplify Button component structure - directly export styled-component

--- client/src/presentational/Button/index.tsx
+++ client/src/presentational/Button/index.tsx
@@ -1,1 +1,19 @@
-export { default as Button } from './Button';
+import styled, { css } from '../../theme';
+
+export const Button = styled.button`${({ theme }) => css`
+  background-color: ${theme.primaryColor};
+  border: 1px solid ${theme.borderColor};
+  padding: .5rem;
+  color: ${theme.fontColor};
+  font-size: ${theme.fontSize};
+
+  :hover{
+    transition: background-color .15s linear;
+    background-color: ${theme.hoverEffect(theme.primaryColor)};
+  }
+
+  :active{
+    transition: background-color .01s linear;
+    background-color: ${theme.activeEffect(theme.primaryColor)};
+  }
+`}`
