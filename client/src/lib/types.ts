e27aeb26efd9f0c70b77de8acac61441d06652ce
Epikem <dltldls95@naver.com>
2018-10-26 19:44:42 +0900

    add util types for props

--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -13,3 +13,34 @@ export type Nullable<T> = {
 export type Stringify<T> = {
   [P in keyof T]: string;
 };
+
+export type DivProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export type InputProps = React.DetailedHTMLProps<React.HTMLAttributes<HTMLInputElement>, HTMLInputElement>;
+
+export interface ControlledProps {
+  defaultValue?: string;
+  onChange?: React.FormEventHandler;
+  value?: string;
+  placeholder?: string;
+}
+
+export type InputChangeEvent = (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+// // from styled-components typing
+
+// type KeyofBase = keyof any;
+// type Diff<T extends KeyofBase, U extends KeyofBase> = ({ [P in T]: P } &
+//     { [P in U]: never })[T];
+// type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+// export type DiffBetween<T, U> = Pick<T, Diff<keyof T, keyof U>> &
+//     Pick<U, Diff<keyof U, keyof T>>;
+// export type WithOptionalTheme<P extends { theme?: T }, T> = Omit<P, 'theme'> & {
+//     theme?: T;
+// };
+
+// type NonOptionalKeys<T> = { [k in keyof T]-?: undefined extends T[k] ? never : k }[keyof T];
+
+// type PickOptional<T> = {
+//   [P in keyof T]?: T[P];
+// };
