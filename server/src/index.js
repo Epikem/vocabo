05d5ef9c358477ae5a48cff68f58b9a023136537
Epikem <dltldls95@naver.com>
2019-05-14 00:38:35 +0900

    upgrade elasticsearch js client version

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,14 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 app.use(cors());
-var elasticsearch = require('elasticsearch');
+// var elasticsearch = require('elasticsearch');
+var { Client } = require('@elastic/elasticsearch');
+
+const client = new Client({
+    node: {
+        url: new URL(process.env.ELASTICSEARCH_URL)
+    }
+})
 
 require('dotenv').config();
 var client_id = process.env.NAVER_ID;
@@ -58,72 +65,93 @@ app.get('/api/v1.0/autocomplete/elastic/en/:query', async (req, res) => {
     var query = req.params.query;
     console.log(`autocomplete query: ${query}`);
     const host = (getEnv() == 'prod') ? process.env.ELASTIC_URL : process.env.ELASTICSEARCH_URL;
-    var client = new elasticsearch.Client({
-        host,
-        // log: 'trace'
-        apiVersion: '6.6',
-    });
+    // var client = new elasticsearch.Client({
+    //     host,
+    //     // log: 'trace'
+    //     apiVersion: '6.6',
+    // });
 
     var results;
 
-    if (is_hangul_char(query[0])) {
-        // hangul query
-        const response = await client.search({
-            index: 'kengdic',
-            type: 'logs',
-            body: {
-                query: {
-                    match: {
-                        word: query
-                    }
-                }
-            }
-        })
-
-        // console.log(response.hits.hits);
-
-        results = response.hits.hits.map((hit, idx) => {
-            return {
-                id: idx,
-                Korean: hit._source.word,
-                English: hit._source.def
-            }
-        });
-
-        console.log(results);
-
-    } else {
-        // english query
-        const response = await client.search({
-            index: 'kengdic',
-            type: 'logs',
-            body: {
-                suggest: {
-                    defSuggest: {
-                        prefix: query,
-                        completion: {
-                            field: 'def',
+    try {
+        
+    
+        if (is_hangul_char(query[0])) {
+            // hangul query
+            const {body, statusCode, warnings} = await client.search({
+                index: 'kengdic',
+                // type: 'logs',
+                body: {
+                    query: {
+                        match: {
+                            word: query
                         }
                     }
                 }
+            })
+    
+    
+            if(statusCode != 200){
+                console.warn('error');
+                console.warn(warnings)
             }
-        })
-
-        results = response.suggest.defSuggest[0].options.map((option, id) => {
-            return {
-                id,
-                English: option.text,
-                Korean: option._source.word
-            };
-        });
-
-        console.log(results);
-
+            // console.log(response.hits.hits);
+    
+            results = body.hits.hits.map((hit, idx) => {
+                return {
+                    id: idx,
+                    Korean: hit._source.word,
+                    English: hit._source.def
+                }
+            });
+    
+            console.log(results);
+    
+        } else {
+            // english query
+            const { body, statusCode, warnings } = await client.search({
+                index: 'kengdic',
+                // type: 'logs',
+                body: {
+                    suggest: {
+                        defSuggest: {
+                            prefix: query,
+                            completion: {
+                                field: 'def',
+                            }
+                        }
+                    }
+                }
+            })
+    
+            if(statusCode != 200){
+                console.warn('error');
+                console.warn(warnings)
+            }
+    
+            results = body.suggest.defSuggest[0].options.map((option, id) => {
+                return {
+                    id,
+                    English: option.text,
+                    Korean: option._source.word
+                };
+            });
+    
+            console.log(results);
+    
+        }
+    
+        res.status(200);
+        res.json(results);
+        res.end();
+    } catch (error) {
+        console.warn(error);
+        console.warn('error!!!')
+        console.warn(JSON.stringify(error.meta.body.error))
+        res.status(500);
+        res.json({});
+        res.end();
     }
-
-    res.status(200);
-    res.json(results);
-    res.end();
 
 })
 
